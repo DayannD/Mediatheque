cabedad34293f72399a9a2e513bc170c921dd027
Dayann <dayanndeshayes@gmail.com>
2021-10-16 18:21:38 +0200

    création de ma page home + test fonctionnel

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Livre;
+use App\Repository\LivreRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(PaginatorInterface $paginator,Request $request): Response
+    {
+        $data = new Livre();
+
+        $data = $this->getDoctrine()->getRepository(Livre::class)->findAll();
+
+        $livre = $paginator->paginate(
+            $data,
+            $request->query->getInt('page',1),
+            8
+        );
+
+        return $this->render('home/index.html.twig', [
+            'livre' => $livre,
+        ]);
+    }
+}
---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+
+class HomeControllerTest extends WebTestCase
+{
+
+  public function testHomePage()
+  {
+    $client = static::createClient();
+    $client->request('GET','/');
+    $this->assertResponseStatusCodeSame(HttpFoundationResponse::HTTP_OK);
+  }
+
+  public function testNavPage()
+  {
+    $client = static::createClient();
+    $client->request('GET','/');
+    $this->assertSelectorTextContains('h1','Bienvenue');
+  }
+}
